c8d0040021c5d96fffe68dff7fb1d04d06443862
Nazim Boudeffa <boudeffa@gmail.com>
2022-07-07 15:08:54 +0200

    Started using the API with axios

--- src/components/RegisterPage.vue
+++ src/components/RegisterPage.vue
@@ -1,18 +1,18 @@
 <template>
-    <div class="login-form">
+    <div class="login-form" @submit.prevent="handleSubmit">
         <form action="" method="post" id="registerForm">
             <h2 class="text-center">Register</h2>
             <div class="form-group">
-                <input type="text" name="rusername" id="rusername" class="form-control validate[required,minSize[3],custom[onlyLetterNumber]]" placeholder="Username" required="required">
+                <input type="text" name="rusername" id="rusername" v-model="username" class="form-control validate[required,minSize[3],custom[onlyLetterNumber]]" placeholder="Username" required="required">
             </div>
                     <div class="form-group">
-                            <input type="text" name="remail" id="remail" class="form-control validate[required,custom[email]]" placeholder="Email" required="required">
+                            <input type="text" name="remail" id="remail" v-model="email" class="form-control validate[required,custom[email]]" placeholder="Email" required="required">
                     </div>
             <div class="form-group">
-                <input type="password" name="rpassword" id="rpassword" class="form-control validate[minSize[6],maxSize[15]]" placeholder="Password" required="required">
+                <input type="password" name="rpassword" id="rpassword" v-model="password" class="form-control validate[minSize[6],maxSize[15]]" placeholder="Password" required="required">
             </div>
                     <div class="form-group">
-                            <input type="password" name="rcpassword" id="rcpassword" class="form-control validate[minSize[6],maxSize[15]]" placeholder="Confirm Password" required="required">
+                            <input type="password" name="rcpassword" id="rcpassword" v-model="cpassword" class="form-control validate[minSize[6],maxSize[15]]" placeholder="Confirm Password" required="required">
                     </div>
             <div class="form-group">
                 <button type="submit" class="btn btn-primary btn-block">Sign in</button>
@@ -22,8 +22,38 @@
 </template>
 
 <script>
+    import axios from 'axios'
     export default {
-        name: 'RegisterPage'
+        name: 'RegisterPage',
+        data() {
+            return {
+                username: '',
+                email: '',
+                password: '',
+                cpassword: ''
+            }
+        },
+        methods: {
+            handleSubmit(){
+                const data = {
+                    //username: this.username,
+                    email: this.email,
+                    password: this.password,
+                    //cpassword: this.cpassword 
+                }
+                
+                axios.post('http://localhost:5000/api/user/register', data)
+                .then(
+                    res => {
+                        console.log(res)
+                    }
+                ).catch(
+                    err => {
+                        console.log(err)
+                    }
+                )
+            }
+        }
     }
 </script>
 
